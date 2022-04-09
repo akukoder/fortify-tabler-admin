8487d900d1f08e50526c714ec3ada5836059cc05
Syahril Zulkefli <syahzul@gmail.com>
2022-04-09 19:16:48 +0800

    Update status

--- stubs/resources/views/components/status.blade.php
+++ stubs/resources/views/components/status.blade.php
@@ -4,6 +4,9 @@
             @break
         @case('two-factor-authentication-enabled')
             @break
+        @case('profile-information-updated')
+            <div class="alert alert-info">{{ __('Profile information updated!') }}</div>
+            @break
         @default
             <div class="alert alert-info">{{ session('status') }}</div>
     @endswitch
