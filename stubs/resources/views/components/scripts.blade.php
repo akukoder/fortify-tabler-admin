46d5479a6e7fdaf929fb2c9ac35175068e861b9a
Syahril Zulkefli <syahzul@gmail.com>
2022-04-10 18:55:03 +0800

    Added default Laravel error pages

---
+++ stubs/resources/views/components/scripts.blade.php
@@ -0,0 +1,3 @@
+<script src="{{ mix('js/app.js') }}"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.slim.js" integrity="sha512-HNbo1d4BaJjXh+/e6q4enTyezg5wiXvY3p/9Vzb20NIvkJghZxhzaXeffbdJuuZSxFhJP87ORPadwmU9aN3wSA==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+{{ $js ?? '' }}
