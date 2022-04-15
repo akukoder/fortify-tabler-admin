14c924c851bc93bec751aa8211d5d7c1613b086b
Syahril Zulkefli <syahzul@gmail.com>
2022-04-15 10:39:47 +0800

    Fix view, use new component style layout

--- stubs/resources/views/auth/verify-email.blade.php
+++ stubs/resources/views/auth/verify-email.blade.php
@@ -1,24 +1,22 @@
-@extends('layouts.auth')
+<x-layouts.auth>
+    @if (session('status') == 'verification-link-sent')
+        <div class="alert alert-success">
+            {{ __('auth.resend_confirmation') }}
+        </div>
+    @endif
 
-@section('content')
-@if (session('status') == 'verification-link-sent')
-<div class="alert alert-success">
-    {{ __('auth.resend_confirmation') }}
-</div>
-@endif
-
-<div class="card card-md">
-    <div class="card-body">
-        <h2 class="text-center">{{ __('auth.email_verification') }}</h2>
-        <p class="text-center mx-2">
-            {{ __('auth.email_verification_text') }}
-        </p>
-        <form method="POST" action="{{ route('verification.send') }}">
-            @csrf
-            <div class="">
-                <button type="submit" class="btn btn-primary w-100" tabindex="2">{{ __('auth.resend') }}</button>
-            </div>
-        </form>
+    <div class="card card-md">
+        <div class="card-body">
+            <h2 class="text-center">{{ __('auth.email_verification') }}</h2>
+            <p class="text-center mx-2">
+                {{ __('auth.email_verification_text') }}
+            </p>
+            <form method="POST" action="{{ route('verification.send') }}">
+                @csrf
+                <div class="">
+                    <button type="submit" class="btn btn-primary w-100" tabindex="2">{{ __('auth.resend') }}</button>
+                </div>
+            </form>
+        </div>
     </div>
-</div>
-@endsection
+</x-layouts.auth>
