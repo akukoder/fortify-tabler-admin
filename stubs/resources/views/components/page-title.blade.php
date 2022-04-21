9200064b066454df84b6392470f4a489d65b86bd
Syahril Zulkefli <syahzul@gmail.com>
2022-04-21 11:33:18 +0800

    If the vars is empty, do not show the header at all

--- stubs/resources/views/components/page-title.blade.php
+++ stubs/resources/views/components/page-title.blade.php
@@ -1,28 +1,30 @@
 @if (isset($pretitle) OR isset($title) OR isset($actions) OR isset($subtitle))
-    <div class="container-xl">
-        <!-- Page title -->
-        <div class="page-header d-print-none">
-            <div class="row align-items-center">
-                <div class="col">
-                    @if (! empty($pretitle))
-                        <div class="page-pretitle">
-                            {{ $pretitle ?? 'Overview' }}
-                        </div>
-                    @endif
+    @if (!empty($pretitle) OR !empty($title) OR !empty($actions) OR !empty($subtitle))
+        <div class="container-xl">
+            <!-- Page title -->
+            <div class="page-header d-print-none d-none d-md-block">
+                <div class="row align-items-center">
+                    <div class="col">
+                        @if (! empty($pretitle))
+                            <div class="page-pretitle">
+                                {{ $pretitle ?? 'Overview' }}
+                            </div>
+                        @endif
 
-                    @if (! empty($title))
-                        <h2 class="page-title">
-                            {{ $title ?? 'Dashboard' }}
-                        </h2>
-                    @endif
+                        @if (! empty($title))
+                            <h2 class="page-title">
+                                {{ $title ?? 'Dashboard' }}
+                            </h2>
+                        @endif
 
-                    @if (! empty($subtitle))
-                        {{ $subtitle ?? '' }}
-                    @endif
+                        @if (! empty($subtitle))
+                            {{ $subtitle ?? '' }}
+                        @endif
+                    </div>
+
+                    {{ $actions ?? '' }}
                 </div>
-
-                {{ $actions ?? '' }}
             </div>
         </div>
-    </div>
+    @endif
 @endif
